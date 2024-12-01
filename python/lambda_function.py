6402366cfebb15aa0762b7122df0132cf2a11b54
Joel <erni.joel@hotmail.com>
2024-12-01 16:09:23 +0100

    inserting raw weather data works

--- python/lambda_function.py
+++ python/lambda_function.py
@@ -2,7 +2,7 @@ import json
 import create_raw_tables
 import CONSTANTS
 import insert_demo_data_into_table
-
+import insert_weather_into_table
 
 def lambda_handler(event, context):
 
@@ -10,8 +10,10 @@ def lambda_handler(event, context):
     # create_raw_tables.create_tables()
 
     print("LAMBDA_HANDLER: Inserting data into the database...")
-    path = CONSTANTS.BASE_DATA_PATH + "/" + CONSTANTS.DEMOGRAPHICS_PREFIX + ".csv"
-    insert_demo_data_into_table.load_demographics_data_to_db(path)
+    # path = CONSTANTS.BASE_DATA_PATH + "/" + CONSTANTS.DEMOGRAPHICS_PREFIX + ".csv"
+    # insert_demo_data_into_table.load_demographics_data_to_db(path)
+
+    insert_weather_into_table.insert_weather_data()
 
     return {
         'statusCode': 200,
