c7d5ae338452ae5331788653a199c072f72b02de
Joel <erni.joel@hotmail.com>
2025-01-02 15:02:23 +0100

    readded constants

--- python/helpers/CONSTANTS.py
+++ python/helpers/CONSTANTS.py
@@ -32,7 +32,7 @@ def set_aws_data():
 # ========== PASSWORD SECTION ==========
 
 # Database
-DB_HOST_ENDPOINT = 'rawdatadb.cl0q24wcqwfj.us-east-1.rds.amazonaws.com'
+DB_HOST_ENDPOINT = 'rawdatadb.cl0q[...]jazonaws.com'
 DB_NAME = 'rawdatadb'
 DB_PORT = 5432
 DB_USER = 'postgres'
@@ -48,9 +48,9 @@ AWS_REGION = 'us-east-1'
 AWS_TOKEN_STRING = \
 """
 [default]
-aws_access_key_id=ASIAWIQOUAXGNESJAWHS
-aws_secret_access_key=6d8r3eWw5Ay3qV6b573gmzyIiLYG80BTsi40h1sp
-aws_session_token=IQoJb3JpZ2luX2VjEGAaCXVzLXdlc3QtMiJGMEQCIE9mj0W3+uBQcbBX+sBAXxOy5CyEaUFPllxOjrTua1NIAiAcvlx5lrREEpUOAG+j8fZA2hxxrHXe9mGxFgQQcYNiLiqkAggZEAEaDDQzMDYwMTE0Mzc1NiIMkOt9yqr4wKAGpv1cKoECOnrAh2P8Gm4HuWfnUH2sYoHDQjrDMhsdXbk6ID1b7kKBo2+Zj+bVk+oV4dFbvunUYfGdpx3ffTpFMT7VSucXe2y6yG86HayB30O/3uuN3phlQ44fqJfwzGaZMSosV4qVS9NsgGq7d1ojBDShxtqw1Z7eH1CW2AiIle8w93L0XtiuC34AKK5xeuHKGskFUPQVP3+QOLPxOtZCvNMQXXpKQ9UeculQ4fMjVZGhGPnO+dGYuU/uen4HT++Ja7ikLACZSwMJ/OfuzZGdZd7bO1ZfJZtG8sBBoyR1DvwC7oPReOBJENG05W9Lkm6VnqaYt9bWOsD0P9Px2a+EeUK6vv/vmQMw3ZbHugY6ngGa/z3zuqLtdCOv2IIfe9EQLX1cOdHZd71nlnWRf+cxchaPiak3mrvUySzxlTrfsZ1UB053yizpvdJm0I6HPOsFOVGtaGi6hWdPRF5cVUwzb+5DXGFfEKumD7GR5NLjnjrndIZZFp+ZTC4GWr/re/ywnphuPG6vrG50ezOjvNH13vvADcPAC6A3scYIwIQL3mXuxPMnFDsHHranFtioXA==
+aws_access_key_id=somekey
+aws_secret_access_key=someaccesskey
+aws_session_token=sometoken
 """
 
 # ======= END OF PASSWORD SECTION =======
